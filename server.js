702317491787f2c2c47e6cad820957f3a3535078
stewk033 <kevin.c.stewart04@gmail.com>
2022-01-26 22:06:17 -0500

    handle requests/file structure

--- server.js
+++ server.js
@@ -1,2 +1,16 @@
+const path = require('path');
+const fs = require('fs');
 const express = require('express');
+const { endianness } = require('os');
 const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+
+app.get('/:wildcard', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now on port 3001!`);
+});
