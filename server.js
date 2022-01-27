9ffe3ad6af85f09d76533975d71ef25adc56a805
stewk033 <kevin.c.stewart04@gmail.com>
2022-01-26 22:41:52 -0500

    push to heroku

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const { endianness } = require('os');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
 // INDEX.HTML
 
 app.get('/', (req, res) => {
@@ -18,6 +21,31 @@ app.get('/api/notes', (req, res) => {
     res.json(JSON.parse(fs.readFileSync('./db/db.json') || []));
 });
 
+// POST/CREATE route for notes
+
+app.post('/api/notes', (req, res) => {
+    let notes = JSON.parse(fs.readFileSync('./db/db.json') || []);
+    let newNote = {
+      id: notes.length == 0 ? 1 : Math.max(...notes.map(note => note.id)) + 1,
+      ...req.body,
+    };
+    notes.push(newNote);
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+    res.json(notes);
+});
+
+// DELETE route for notes
+
+app.delete('/api/notes/:id', (req, res) => {
+    if (!req.params.id) res.end();
+    let notes = JSON.parse(fs.readFileSync('./db/db.json') || []);
+    let noteIndex = notes.findIndex(note => note.id === +req.params.id);
+    let note = notes.splice(noteIndex, 1);
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+    res.send(note);
+});
+
+// REDIRECT TO INDEX
 
 app.get('/:wildcard', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
